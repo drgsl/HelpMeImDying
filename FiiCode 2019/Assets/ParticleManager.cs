428129bc64ebb248f94090880f7919c096bc7114
BobuDragos <bobudragos1@gmail.com>
2019-05-06 17:29:35 +0300

    After Contest

---
+++ FiiCode 2019/Assets/ParticleManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleManager : MonoBehaviour
+{
+
+    public GameObject[] particles;
+
+    GameObject player;
+
+    bool[] activated = new bool[10];
+
+    public GameObject[] plantSets;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        for (int i = 0; i < particles.Length; i++)
+        {
+                activated[i] = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkParticle(20, 0);
+        checkParticle(40, 1);
+        checkParticle(60, 2);
+        checkParticle(80, 3);
+        checkParticle(100, 4);
+        checkParticle(110, 5);
+    }
+
+    void checkParticle(int level, int number)
+    {
+        if (Changeable.level >= level && activated[number] == false)
+        {
+            Instantiate(particles[number], player.transform.position, player.transform.rotation);
+            activated[number] = true;
+            plantSets[number].SetActive(true);
+        }
+    }
+}
---
+++ FiiCode 2019/Assets/_Scripts/Changeable.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Changeable : MonoBehaviour
+{
+
+    public GameObject changed;
+    public int value;
+    public int neededlvl;
+    public static int level = -1;
+    Text scoretext;
+    public string changetext = "Press E to help the planet";
+
+    FPS fps;
+
+    Animator CharAnim;
+
+    public GameObject[] particles;
+
+
+    private void Awake()
+    {
+        scoretext = GameObject.Find("ScoreText").GetComponentInChildren<Text>();
+        fps = GameObject.FindGameObjectWithTag("Player").GetComponent<FPS>();
+
+        CharAnim = fps.gameObject.GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        scoretext.text = "" + level +" Eco - Points";
+    }
+
+    public void Change()
+    {
+        if (level >= neededlvl)
+        {
+            CharAnim.SetTrigger("PickUp");
+            level += value;
+            //GameObject newobj = 
+            Instantiate(changed, transform.position, transform.rotation);
+            fps.SavePlayer();
+            //newobj.transform.localScale = gameObject.transform.localScale;
+            Destroy(gameObject);
+        }
+    }
+}
